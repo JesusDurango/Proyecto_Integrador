a09dc12748ca0a0dc358dd68f054cb6613faedda
Jesus Abrahan <jesus.abrahanda@gmail.com>
2023-08-02 18:48:30 -0500

    se agrega la parte dos del proyecto integrador

--- Main.py
+++ Main.py
@@ -1,4 +1,20 @@
+import readchar
+
 # Proyecto integrador V.1
 print("Por favor introduce tu nombre: ")
 nombre = input ()
 print (f"Hola, bienvenido a una nueva partida {nombre}.")
+
+# Proyecto integrador V.2
+
+def main():
+    
+    while True:
+        tecla_presionada = readchar.readkey()
+        print(f"Tecla presionada: {tecla_presionada}")
+        if tecla_presionada == readchar.key.UP:  
+            break
+        
+if __name__ == "__main__":
+     print("Presiona la tecla que desees y solo presiona la tecla ↑ (UP) para terminar el programa.")
+     main()
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Proyecto_Integrador
+Primera parte del proyecto integrador el objetivo es crear un codigo que permita:
+* Pedir el nombre del jugador por teclado
+* Imprimir un mensaje de bienvenida con el nombre
